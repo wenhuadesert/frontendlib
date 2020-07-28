f8b6fbc263139de13e224cfda25f485a835f2a94
1170300728 <2964175191@qq.com>
2020-07-28 20:31:09 +0800

    TTN 添加丢失文件

--- src/components/employee/add.vue
+++ src/components/employee/add.vue
@@ -3,7 +3,7 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">增加部门</h3>
+          <h3 class="box-title">增加员工</h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
@@ -14,15 +14,27 @@
           </div>
         </div>
         <div class="box-body">
-          	<form action="add.mvc" method = post>
+          	<form action="add.mvc" method = post v-on:submit="add()">
 			  <div class="form-group">
-			    <label for="exampleInputEmail1">部门名称</label>
-			    <input type="text" class="form-control" name="name">
+			    <label for="exampleInputPassword1">员工编号</label>
+			    <input type="text" class="form-control" v-model="employee.id" required="">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">员工姓名</label>
+			    <input type="text" class="form-control" v-model="employee.name" required="">
 			    <small id="emailHelp" class="form-text text-muted"></small>
 			  </div>
 			  <div class="form-group">
-			    <label for="exampleInputPassword1">部门编号</label>
-			    <input type="text" class="form-control" name="code">
+			    <label for="exampleInputPassword1">员工性别</label>
+			    <input type="text" class="form-control" v-model="employee.sex" required="">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">员工用户名</label>
+			    <input type="text" class="form-control" v-model="employee.username" required="">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">员工密码</label>
+			    <input type="text" class="form-control" v-model="employee.password" required="">
 			  </div>
 			  <button type="submit" class="btn btn-primary">增加</button>
 			  <router-link to="/employee/list" class="btn btn-default">取消</router-link>
@@ -37,7 +49,20 @@
 	export default{
 		name:"EmployeeAdd",
 		data(){
-			return{};
+			return{
+				employee:{
+					id:"",
+					name:"",
+					sex:"",
+					username:"",
+					password:""
+				}
+			};
+		},
+		methods:{
+			add(){
+				
+			}
 		}
 	}
 </script>
---
+++ src/components/employee/main.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<RouterView></RouterView>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+</style>
