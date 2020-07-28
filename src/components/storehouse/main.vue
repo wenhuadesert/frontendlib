deaa20637f0f4fa4011c4fcdcdc79ed446775e80
陈峥权 <1758388971@qq.com>
2020-07-28 20:14:15 +0800

    陈峥权 增加了仓库添加功能

--- src/components/storehouse/main.vue
+++ src/components/storehouse/main.vue
@@ -1,39 +1,26 @@
 <template>
-	<div class="box">
-	  <div class="box-header with-border">
-	    <h3 class="box-title">仓库管理</h3>
-	   
-	  </div>
-	  <div class="box-body">
-	 	<table class="table table-bordered">
-					  <thead>
-					    <tr>
-					      <th scope="col">编号</th>
-					      <th scope="col">编码</th>
-					      <th scope="col">名称</th>
-					      <th scope="col">操作</th>
-					    </tr>
-					  </thead>
-					  <tbody>
-					 
-					    <tr>
-					      <td></td>
-					      <td></td>
-					      <td></td>
-					      <td><a href="tomodify.mvc" class="btn btn-default">修改</a>
-					          <a href="todelete.mvc" class="btn btn-danger">删除</a> 
-					          <a href="toview.mvc" class="btn btn-default">查看</a>  </td>
-					    </tr>
-					  
-					  </tbody>
-					</table>
-	  </div>
-	  <!-- /.box-body -->
-	  <a href="toadd.mvc" class="btn btn-default">增加新仓库</a>
-	</div>
+<div>
+	<RouterView></RouterView>	
+</div>
+
 </template>
 
 <script>
+	//部门的主组件
+	export default {
+		name:"DepartmentMain",
+		data(){
+			return {
+				
+			};
+		},
+		created(){ //当前组件的生命周期方法，组件创建后
+			
+		},
+		methods:{
+			
+		}
+	}
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,13 @@ import VueRouter from 'vue-router'
 import HomeMain from "./../components/home/main.vue";
 import EmployeeLogin from "./../components/employee/login.vue";
 import EmployeeList from "./../components/employee/list.vue";
+
+//仓库路由
 import StorehouseMain from "./../components/storehouse/main.vue";
+import StorehouseList  from "./../components/storehouse/list.vue";
+import StorehouseAdd  from "./../components/storehouse/add.vue";
+import StorehouseModify  from "./../components/storehouse/modify.vue";
+import StorehouseView  from "./../components/storehouse/view.vue";
 
 
 Vue.use(VueRouter)
@@ -14,7 +20,13 @@ const routes = [
 	{path:"/", name:"homemain", component:HomeMain},
 	{path:"/employee/login", name:"employeelogin", component:EmployeeLogin},
 	{path:"/employee", name:"employeelist", component:EmployeeList},
-	{path:"/storehouse", name:"storehousemain", component:StorehouseMain},
+	{path:"/storehouse", name:"storehousemain", component:StorehouseMain,children:[
+		{path:"list",name:"storehouselist",component:StorehouseList},
+		{path:"add",name:"storehouseadd",component:StorehouseAdd},
+		{path:"modify/:storehouseId",name:"storehousemodify",component:StorehouseModify},
+		{path:"view/:storehouseId",name:"storehouseview",component:StorehouseView},
+		{path:"", redirect: "list" }
+	]}
 ]
 
 const router = new VueRouter({
